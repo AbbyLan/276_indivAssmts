fe01c1f7dc0dbe3b9e5c61cee0446759a91e34ff
Haochen Yang <haochenyang@HaochendeMacBook-Pro.local>
2022-02-16 01:51:38 -0800

    connect to database

--- index.js
+++ index.js
@@ -2,9 +2,36 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/database', (req,res) => {
+  var getRectanglesQuery = `SELECT * FROM rectangle`;
+  pool.query(getRectanglesQuery, (error,result) => {
+    if (error){
+      res.send(error);
+    }
+    var results = {'rows':result.rows}
+    res.render('pages/db', results);
+  })
+})
+app.post('/adduser', (req,res) => {
+  console.log("post request for /adduser");
+  var recName = req.body.recName;
+  var age = req.body.age;
+  res.send(`rectanglename: ${recName}`)
+})
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
