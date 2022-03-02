837956c3af25bda222dbe1d7a6cc27d883e8b764
Haochen Yang <haochenyang@HaochendeMacBook-Pro.local>
2022-03-01 18:29:22 -0800

    fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ pool.connect();
 let app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
